3aa7669f099755fc5f91266ee51332a3ac495ef3
macmogu <mm@moguf2k.com>
2024-11-25 00:25:23 +0800

    adjust mapkey

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -38,17 +38,56 @@ map(
 -- map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
 
 -- rustaceanvim
-map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
+-- map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
 
 -- Go related mappings
 map("n", "<leader>gr", "<cmd>GoRun<cr>", { desc = "Go Run" })
-map("n", "<leader>gt", "<cmd>GoTest<cr>", { desc = "Go Test" })
+-- map("n", "<leader>gt", "<cmd>GoTest<cr>", { desc = "Go Test" })
 map("n", "<leader>gi", "<cmd>GoImport<cr>", { desc = "Go Import" })
-map("n", "<leader>gf", "<cmd>GoFmt<cr>", { desc = "Go Format" })
+-- map("n", "<leader>gf", "<cmd>GoFmt<cr>", { desc = "Go Format" })
+-- map("n", "<leader>gtf", ":GoTestFile<CR>", { desc = "Test current file" })
+-- map("n", "<leader>gtc", ":GoTestFunc<CR>", { desc = "Test current function" })
 
--- Go debug related mappings
-map("n", "<leader>dgt", "<cmd>lua require'dap-go'.debug_test()<CR>", { desc = "Debug go test" })
-map("n", "<leader>dgl", "<cmd>lua require'dap-go'.debug_last()<CR>", { desc = "Debug last go test" })
+-- go test -v ./<relative-path-to-file>
+map("n", "<leader>gtf", ":lua require('go.test').test_file()<CR>", { desc = "Test current file" })
+-- go test -run ^TestFunctionName$ ./<relative-path-to-file>
+map("n", "<leader>gtc", ":lua require('go.test').test_func()<CR>", { desc = "Test current function" })
+-- go test -bench=. ./<relative-path-to-file>
+map("n", "<leader>gbf", ":lua require('go.test').benchmark_file()<CR>", { desc = "Benchmark current file" })
+-- go test -bench ^BenchmarkFunctionName$ ./<relative-path-to-file>
+map("n", "<leader>gbc", ":lua require('go.test').benchmark_func()<CR>", { desc = "Benchmark current function" })
+
+local function run_test_or_benchmark(mode)
+  local go_test = require "go.test"
+
+  if mode == "file" then
+    -- 运行当前文件的测试和基准
+    vim.ui.select({ "Test File", "Benchmark File" }, { prompt = "Select action:" }, function(choice)
+      if choice == "Test File" then
+        go_test.test_file()
+      elseif choice == "Benchmark File" then
+        go_test.benchmark_file()
+      end
+    end)
+  elseif mode == "func" then
+    -- 运行当前函数的测试和基准
+    vim.ui.select({ "Test Function", "Benchmark Function" }, { prompt = "Select action:" }, function(choice)
+      if choice == "Test Function" then
+        go_test.test_func()
+      elseif choice == "Benchmark Function" then
+        go_test.benchmark_func()
+      end
+    end)
+  end
+end
+
+-- 测试和基准测试的快捷键
+map("n", "<leader>gf", function()
+  run_test_or_benchmark "file"
+end, { desc = "Run test/benchmark for current file" })
+map("n", "<leader>gc", function()
+  run_test_or_benchmark "func"
+end, { desc = "Run test/benchmark for current function" })
 
 -- Python related mappings
 map("n", "<leader>pr", "<cmd>!python3 %<cr>", { desc = "run Python file" })
