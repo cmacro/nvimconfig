82c2010c8b8c7554da5fe8ef2879ec01d919465c
macmogu <mm@moguf2k.com>
2025-01-06 20:30:14 +0800

    remove go plug commit code

--- lua/plugins/dap-go.lua
+++ lua/plugins/dap-go.lua
@@ -5,20 +5,6 @@ return {
     ft = "go",
     build = ":GoUpdateBinaries",
   },
-  -- {
-  --   "ray-x/go.nvim",
-  --   dependencies = {
-  --     "ray-x/guihua.lua",
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   config = function()
-  --     require("go").setup()
-  --   end,
-  --   event = { "CmdlineEnter" },
-  --   ft = { "go", "gomod" },
-  --   build = ':lua require("go.install").update_all_sync()',
-  -- },
   {
     "leoluz/nvim-dap-go",
     ft = "go",
