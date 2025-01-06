a8a8787072b4f540e752c7aa6a948e2a7b6c1bea
macmogu <mm@moguf2k.com>
2025-01-06 20:06:55 +0800

    update go dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -30,34 +30,4 @@ return {
       )
     end,
   },
-  -- Go related config
-  {
-    "fatih/vim-go",
-    ft = "go",
-    build = ":GoUpdateBinaries",
-  },
-  -- {
-  --   "ray-x/go.nvim",
-  --   dependencies = {
-  --     "ray-x/guihua.lua",
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   config = function()
-  --     require("go").setup()
-  --   end,
-  --   event = { "CmdlineEnter" },
-  --   ft = { "go", "gomod" },
-  --   build = ':lua require("go.install").update_all_sync()',
-  -- },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      require("dap-go").setup()
-    end,
-  },
 }
