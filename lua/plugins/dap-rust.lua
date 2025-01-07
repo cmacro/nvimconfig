856bf3fa74a81ba71c62672e267531b0b6e8945a
macmogu <mm@moguf2k.com>
2025-01-07 21:48:40 +0800

    update slint

--- lua/plugins/dap-rust.lua
+++ lua/plugins/dap-rust.lua
@@ -49,21 +49,5 @@ return {
     ft = "slint",
     lazy = true,
     event = { "BufReadPre", "BufNewFile" },
-    -- config = function()
-    --   local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    --   local on_attach = on_attach
-    --
-    --   require("lsp_lines").setup {
-    --
-    --     require("lspconfig")["slint_lsp"].setup {
-    --       cmd = { "slint-lsp" },
-    --       filetypes = { "slint" },
-    --       single_file_support = true,
-    --       capabilities = capabilities,
-    --       on_attach = on_attach,
-    --       settings = {},
-    --     },
-    --  }
-    -- end,
   },
 }
