8f537fb7ed32773730d1e90ce16ce843765a26be
macmogu <mm@moguf2k.com>
2025-01-12 01:03:05 +0800

    update go dap config

--- lua/plugins/dap-go.lua
+++ lua/plugins/dap-go.lua
@@ -11,34 +11,49 @@ return {
     dependencies = {
       "mfussenegger/nvim-dap",
     },
-    opts = function(_, opts)
-      local dap = require "dap"
-
-      dap.adapters.delve = {
-        type = "server",
-        port = "${port}",
-        executable = {
-          command = "dlv",
-          args = { "dap", "-l", "127.0.0.1:${port}" },
-        },
-      }
-
-      -- Ensure we don't blow away existing debug configs
-      dap.configurations.go = dap.configurations.go or {}
-
-      -- Insert a NEW configuration
-      table.insert(dap.configurations.go, {
-        type = "delve",
-        name = "Debug (Display print outputs)",
-        request = "launch",
-        program = "${file}",
-        outputMode = "remote",
-      })
-
-      return opts
-    end,
+    -- opts = function(_, opts)
+    --   local dap = require "dap"
+    --
+    --   dap.adapters.delve = {
+    --     type = "server",
+    --     port = "${port}",
+    --     executable = {
+    --       command = "dlv",
+    --       args = { "dap", "-l", "127.0.0.1:${port}" },
+    --     },
+    --   }
+    --
+    --   -- Ensure we don't blow away existing debug configs
+    --   dap.configurations.go = dap.configurations.go or {}
+    --
+    --   -- Insert a NEW configuration
+    --   table.insert(dap.configurations.go, {
+    --     type = "delve",
+    --     name = "Debug (Display print outputs)",
+    --     request = "launch",
+    --     program = "${file}",
+    --     outputMode = "remote",
+    --   })
+    --
+    --   return opts
+    -- end,
     -- config = function()
     --   require("dap-go").setup()
     -- end,
+    config = function()
+      local dap_go = require "dap-go"
+      local dap = require "dap"
+
+      dap_go.setup()
+
+      dap.configurations.go = dap.configurations.go or {}
+      table.insert(dap.configurations.go, 1, {
+        type = "go",
+        name = "Debug (Display print outputs)",
+        request = "launch",
+        program = "${file}",
+        outputMode = "remote", -- capturing the output of fmt.Print
+      })
+    end,
   },
-  }
+}
