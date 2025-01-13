5f9f6694132b897cef57e23d707743e80d80a85b
zonvid <fpack@163.com>
2025-01-13 10:27:05 +0800

    use debug command open vim-go

--- lua/plugins/dap-go.lua
+++ lua/plugins/dap-go.lua
@@ -1,10 +1,11 @@
 return {
   -- Go related config
-  -- {
-  --   "fatih/vim-go",
-  --   ft = "go",
-  --   build = ":GoUpdateBinaries",
-  -- },
+  {
+    -- debug command
+    "fatih/vim-go",
+    ft = "go",
+    build = ":GoUpdateBinaries",
+  },
   {
     "leoluz/nvim-dap-go",
     ft = { "go" },
