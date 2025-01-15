4e955aa4bd0245af686602b9e703918b621da4ab
zonvid <fpack@163.com>
2025-01-15 13:08:55 +0800

    remove dap go outputMode debug

--- lua/plugins/dap-go.lua
+++ lua/plugins/dap-go.lua
@@ -43,18 +43,18 @@ return {
     -- end,
     config = function()
       local dap_go = require "dap-go"
-      local dap = require "dap"
-
       dap_go.setup()
 
-      dap.configurations.go = dap.configurations.go or {}
-      table.insert(dap.configurations.go, 1, {
-        type = "go",
-        name = "Debug (Display print outputs)",
-        request = "launch",
-        program = "${file}",
-        outputMode = "remote", -- capturing the output of fmt.Print
-      })
+      -- local dap = require "dap"
+      -- #109 fixed
+      -- dap.configurations.go = dap.configurations.go or {}
+      -- table.insert(dap.configurations.go, 1, {
+      --   type = "go",
+      --   name = "Debug (Display print outputs)",
+      --   request = "launch",
+      --   program = "${file}",
+      --   outputMode = "remote", -- capturing the output of fmt.Print
+      -- })
     end,
   },
 }
